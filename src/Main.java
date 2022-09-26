61dfbcc5dc559bf9195467eb3b7c940e60d7ab0b
pc <mark1337@internet.ru>
2022-09-26 19:31:02 +0200

    Выполненное ДЗ по методам (попытка номер 2)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int year = 2022;
+        int year = 2022; //s
         calculateLeapYear(year);
         int clientOs = 0;
         int clientDeviceYear = 2022;
